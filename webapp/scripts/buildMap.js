d1d3cfb4848b6039eaface22ea3f71cfe4fbbcf6
Gabriele Prestifilippo <gabriele.prestifilippo@welld.ch>
2021-02-11 21:53:12 +0100

    feat: change app path

--- webapp/scripts/buildMap.js
+++ webapp/scripts/buildMap.js
@@ -5,19 +5,19 @@ const configuration = require("./../src/configuration.json");
 const fetchServerFiles = async ({
   basePath,
   inputFilesPath,
-//   municipalities,
+  //   municipalities,
   provinces,
   regions,
 }) => {
-//   const file = await fetch(basePath + inputFilesPath + municipalities);
-//   const fileJSON = await file.json();
-//   const mapped = fileJSON; //map data to municipalitiesMap.json
-//   fs.writeJSONSync(
-//     `./src/static/boundaries/limits_IT_municipalities.json`,
-//     mapped
-//   );
+  //   const file = await fetch(basePath + inputFilesPath + municipalities);
+  //   const fileJSON = await file.json();
+  //   const mapped = fileJSON; //map data to municipalitiesMap.json
+  //   fs.writeJSONSync(
+  //     `./src/static/boundaries/limits_IT_municipalities.json`,
+  //     mapped
+  //   );
 
-//   //todo add mapping to make 'municipalitiesMap.json'
+  //   //todo add mapping to make 'municipalitiesMap.json'
   const provincesPromise = fetchAndWrite(
     basePath + inputFilesPath,
     provinces,
