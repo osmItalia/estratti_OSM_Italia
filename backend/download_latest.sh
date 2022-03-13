8c592d6a237618350a0a5b79dcd5ee5f3f9a8964
Francesco Frassinelli <fraph24@gmail.com>
2022-03-13 20:33:56 +0100

    Fix log handling

--- backend/download_latest.sh
+++ backend/download_latest.sh
@@ -12,24 +12,25 @@ wget -O $WORK_DIR/input/pbf/state.txt http://download.geofabrik.de/$COUNTRY_STAT
 cd $WORK_DIR/output/scripts;
 chmod +x *.sh;
 
-nohup ./regioni_poly.sh > regioni_poly.log;
-nohup ./regioni_bbox.sh > regioni_bbox.log;
-nohup ./province_poly.sh > province_poly.log;
-nohup ./province_bbox.sh > province_bbox.log;
-nohup ./comuni_poly.sh > comuni_poly.log;
-nohup ./comuni_bbox.sh > comuni_bbox.log;
+./regioni_poly.sh &> regioni_poly.log
+./regioni_bbox.sh &> regioni_bbox.log
+./province_poly.sh &> province_poly.log
+./province_bbox.sh &> province_bbox.log
+./comuni_poly.sh &> comuni_poly.log
+./comuni_bbox.sh &> comuni_bbox.log
 
 # per estrarre in formato OSMAND
 cd $WORK_DIR/input/osmand;
 ./osmand-regioni.sh
 
 cd /srv/estratti/output/scripts;
-nohup ./convert_regioni_poly.sh > convert_regioni_poly.log;
-nohup ./convert_regioni_bbox.sh > convert_regioni_bbox.log;
-nohup ./convert_province_poly.sh > convert_province_poly.log;
-nohup ./convert_province_bbox.sh > convert_province_bbox.log;
-nohup ./convert_comuni_poly.sh > convert_comuni_poly.log;
-nohup ./convert_comuni_bbox.sh > convert_comuni_bbox.log;
 
 #
 
+cd /srv/estratti/output/scripts
+./convert_regioni_poly.sh &> convert_regioni_poly.log
+./convert_regioni_bbox.sh &> convert_regioni_bbox.log
+./convert_province_poly.sh &> convert_province_poly.log
+./convert_province_bbox.sh &> convert_province_bbox.log
+./convert_comuni_poly.sh &> convert_comuni_poly.log
+./convert_comuni_bbox.sh &> convert_comuni_bbox.log
