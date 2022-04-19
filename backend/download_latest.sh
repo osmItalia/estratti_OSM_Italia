50d14502867e21a6b3934c41f19f8360c6edfdb7
Francesco Frassinelli <fraph24@gmail.com>
2022-04-20 00:36:18 +0200

    Refactor download_latest.sh

--- backend/download_latest.sh
+++ backend/download_latest.sh
@@ -4,27 +4,29 @@ COUNTRY_PBF="europe/italy-latest.osm.pbf"
 COUNTRY_STATE="europe/italy-updates/state.txt"
 
 cd "$WORK_DIR/input/pbf/"
-wget -N https://download.geofabrik.de/$COUNTRY_PBF
-wget -O $WORK_DIR/input/pbf/state.txt https://download.geofabrik.de/$COUNTRY_STATE
+wget -N "https://download.geofabrik.de/$COUNTRY_PBF"
+wget -O "$WORK_DIR/input/pbf/state.txt" "https://download.geofabrik.de/$COUNTRY_STATE"
 
 cd "$WORK_DIR/output/scripts"
 chmod +x *.sh
 
-./regioni_poly.sh
-./regioni_bbox.sh
-./province_poly.sh
-./province_bbox.sh
-./comuni_poly.sh
-./comuni_bbox.sh
+bash -eu ./regioni_poly.sh
+bash -eu ./province_poly.sh
+bash -eu ./comuni_poly.sh
+
+#bash -eu ./regioni_bbox.sh
+#bash -eu ./province_bbox.sh
+#bash -eu ./comuni_bbox.sh
 
 # per estrarre in formato OSMAND
-cd $WORK_DIR/input/osmand
-./osmand-regioni.sh
+cd "$WORK_DIR/input/osmand"
+bash -eu ./osmand-regioni.sh
 
-cd $WORK_DIR/output/scripts
-./convert_regioni_poly.sh
-./convert_regioni_bbox.sh
-./convert_province_poly.sh
-./convert_province_bbox.sh
-./convert_comuni_poly.sh
-./convert_comuni_bbox.sh
+cd "$WORK_DIR/output/scripts"
+bash -eu ./convert_regioni_poly.sh
+bash -eu ./convert_province_poly.sh
+bash -eu ./convert_comuni_poly.sh
+
+#bash -eu ./convert_regioni_bbox.sh
+#bash -eu ./convert_province_bbox.sh
+#bash -eu ./convert_comuni_bbox.sh
