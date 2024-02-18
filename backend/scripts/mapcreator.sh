e1c159c6c84ab9575d7a4ca0b1d7636187eb4209
Francesco Frassinelli <fraph24@gmail.com>
2024-02-18 13:19:24 +0100

    Make OsmMapCreator work with OpenJDK 17

--- backend/scripts/mapcreator.sh
+++ backend/scripts/mapcreator.sh
@@ -4,9 +4,11 @@ MAPCREATOR="$1"
 CONF="$2"
  
 cd "$MAPCREATOR"
+# https://github.com/osmandapp/OsmAnd-tools/issues/484#issuecomment-1315382705
 exec java \
      -Djava.util.logging.config.file=logging.properties \
      -Xms64M -Xmx12G \
+     --add-opens java.base/java.util=ALL-UNNAMED \
      -cp "./OsmAndMapCreator.jar:lib/OsmAnd-core.jar:./lib/*.jar" "net.osmand.util.IndexBatchCreator" \
     "$CONF"
 
